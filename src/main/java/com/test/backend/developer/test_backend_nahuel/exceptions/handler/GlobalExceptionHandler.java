521665b3ef2765717f705c74ae219244bde3c168
leonahuelfrias <leonahuelfrias@gmail.com>
2022-07-23 11:18:50 -0300

    GlobalExceptionHandler was created

--- src/main/java/com/test/backend/developer/test_backend_nahuel/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/test/backend/developer/test_backend_nahuel/exceptions/handler/GlobalExceptionHandler.java
@@ -1,4 +1,46 @@
 package com.test.backend.developer.test_backend_nahuel.exceptions.handler;
 
+import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateDocException;
+import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateExistsException;
+import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateNotExistsException;
+import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateTechnologiesExistsException;
+import com.test.backend.developer.test_backend_nahuel.exceptions.TechnologyExistsException;
+import com.test.backend.developer.test_backend_nahuel.exceptions.TechnologyNotExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CandidateDocException.class)
+    public ResponseEntity<?> candidateDocHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CandidateExistsException.class)
+    public ResponseEntity<?> candidateExistsHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CandidateNotExistsException.class)
+    public ResponseEntity<?> candidateNotExistsHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CandidateTechnologiesExistsException.class)
+    public ResponseEntity<?> candidateTechnologiesExistsHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(TechnologyExistsException.class)
+    public ResponseEntity<?> technologyExistsHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(TechnologyNotExistsException.class)
+    public ResponseEntity<?> technologyNotExistsHandler (Exception e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
