e92843aacfb8e87affe3b7616feae4c15d90b7ea
leonahuelfrias <leonahuelfrias@gmail.com>
2022-07-24 12:44:49 -0300

    Configuration of Swagger

---
+++ src/main/java/com/test/backend/developer/test_backend_nahuel/configuration/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.test.backend.developer.test_backend_nahuel.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.test.backend.developer.test_backend_nahuel"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo metaData() {
+
+        return new ApiInfoBuilder()
+                .title("Ev-Tec Api doc")
+                .description("Technical evaluation of Moby.")
+                .version("0.85")
+                .termsOfServiceUrl("Terms of service")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0%22")
+                .build();
+    }
+}
