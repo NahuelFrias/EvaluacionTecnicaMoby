3e231b6921a986080524385283f67f774c0dde00
leonahuelfrias <leonahuelfrias@gmail.com>
2022-07-25 10:36:28 -0300

    CandidateTechnologiesProjection was created

--- src/main/java/com/test/backend/developer/test_backend_nahuel/projections/CandidateTechnologiesProjection.java
+++ src/main/java/com/test/backend/developer/test_backend_nahuel/projections/CandidateTechnologiesProjection.java
@@ -1,4 +1,27 @@
 package com.test.backend.developer.test_backend_nahuel.projections;
 
-public class CandidateTechnologiesProjection {
+import org.springframework.beans.factory.annotation.Value;
+
+public interface CandidateTechnologiesProjection {
+
+    @Value("#{target.name + ' ' + target.lastname}")
+    String getFullName();
+
+    @Value("#{target.document_type}")
+    String getDocType();
+
+    @Value("#{target.num_document}")
+    String getDocument();
+
+    @Value("#{target.birthdate}")
+    String getBirthdate();
+
+    @Value("#{target.technology_name}")
+    String getTechnologyName();
+
+    @Value("#{target.version}")
+    String getVersion();
+
+    @Value("#{target.experience}")
+    String getExperience();
 }
