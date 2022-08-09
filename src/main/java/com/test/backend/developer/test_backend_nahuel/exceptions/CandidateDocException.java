de4de18546d8a8b86c9e4486c1674b96c6112eef
leonahuelfrias <leonahuelfrias@gmail.com>
2022-08-09 08:51:45 -0300

    TestBackendNahuelApplicationTests and CandidateDocException classes were deleted

--- src/main/java/com/test/backend/developer/test_backend_nahuel/exceptions/CandidateDocException.java
+++
@@ -1,7 +0,0 @@
-package com.test.backend.developer.test_backend_nahuel.exceptions;
-
-public class CandidateDocException extends RuntimeException{
-    public CandidateDocException (String msg){
-        super(msg);
-    }
-}
--- src/test/java/com/test/backend/developer/test_backend_nahuel/TestBackendNahuelApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package com.test.backend.developer.test_backend_nahuel;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestBackendNahuelApplicationTests {
-}
