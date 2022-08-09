eae92836ea08fc974340abb3008cdeaf4e1f6724
leonahuelfrias <leonahuelfrias@gmail.com>
2022-08-09 09:32:50 -0300

    An unused import was deleted

--- src/main/java/com/test/backend/developer/test_backend_nahuel/models/entities/Technology.java
+++ src/main/java/com/test/backend/developer/test_backend_nahuel/models/entities/Technology.java
@@ -12,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
