ba130763aadc6ed6e6dd33bf5c3723e13541fcce
leonahuelfrias <leonahuelfrias@gmail.com>
2022-08-11 14:37:48 -0300

    The test findByIdWhenNotExistsTest was created

--- src/test/java/com/test/backend/developer/test_backend_nahuel/controllers/CandidateTechnologyControllerTests.java
+++ src/test/java/com/test/backend/developer/test_backend_nahuel/controllers/CandidateTechnologyControllerTests.java
@@ -1,35 +1,25 @@
 package com.test.backend.developer.test_backend_nahuel.controllers;
 
 import com.google.gson.Gson;
-import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateExistsException;
-import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateNotExistsException;
 import com.test.backend.developer.test_backend_nahuel.exceptions.CandidateTechnologiesExistsException;
 import com.test.backend.developer.test_backend_nahuel.exceptions.TechnologyNotExistsException;
-import com.test.backend.developer.test_backend_nahuel.models.views.CandidateDTO;
 import com.test.backend.developer.test_backend_nahuel.models.views.CandidateTechnologiesDTO;
 import com.test.backend.developer.test_backend_nahuel.repositories.CandidateTechnologiesRepository;
 import com.test.backend.developer.test_backend_nahuel.services.impl.CandidateTechnologiesServicesImpl;
 import com.test.backend.developer.test_backend_nahuel.services.impl.TechnologyServiceImpl;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Optional;
 
-import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidate;
-import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidateDTO;
-import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidateList;
 import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidateTechnologies;
 import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidateTechnologiesDTO;
 import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getCandidateByTechnologyList;
 import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getTechnology;
-import static com.test.backend.developer.test_backend_nahuel.utils.TestEntityFactory.getTechnologyDTO;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
@@ -56,28 +46,31 @@ class CandidateTechnologyControllerTests extends AbstractMVCTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    void createTest() throws Exception {
-        var candidateTechnologiesDto = getCandidateTechnologiesDTO();
-        String candidateTechnologiesDtoJson = new Gson().toJson(getCandidateTechnologiesDTO());
-        mockMvc.perform(post("/ev-tec/candidate-technologies/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(candidateTechnologiesDtoJson))
-                .andExpect(status().isCreated());
-        verify(candidateTechnologiesService, atLeastOnce()).create(any(CandidateTechnologiesDTO.class));
-    }
+    @Nested
+    class CreateTest {
+        @Test
+        void createTestOk() throws Exception {
+            var candidateTechnologiesDto = getCandidateTechnologiesDTO();
+            String candidateTechnologiesDtoJson = new Gson().toJson(getCandidateTechnologiesDTO());
+            mockMvc.perform(post("/ev-tec/candidate-technologies/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(candidateTechnologiesDtoJson))
+                    .andExpect(status().isCreated());
+            verify(candidateTechnologiesService, atLeastOnce()).create(any(CandidateTechnologiesDTO.class));
+        }
 
-    @Test
-    void createCandidateTechnologiesAlreadyExistTest() throws Exception {
-        doThrow(CandidateTechnologiesExistsException.class).when(candidateTechnologiesService).create(getCandidateTechnologiesDTO());
-        String candidateTechnologiesJson = new Gson().toJson(getCandidateTechnologiesDTO());
-        var candidateTechnology = getCandidateTechnologiesDTO();
-        mockMvc.perform(post("/ev-tec/candidate-technologies/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(candidateTechnologiesJson))
-                .andExpect(status().isAccepted())
-                .andExpect(result -> assertThrows(CandidateTechnologiesExistsException.class, () -> candidateTechnologiesService.create(candidateTechnology)));
-        verify(candidateTechnologiesService, atLeastOnce()).create(any(CandidateTechnologiesDTO.class));
+        @Test
+        void createCandidateTechnologiesAlreadyExistTest() throws Exception {
+            doThrow(CandidateTechnologiesExistsException.class).when(candidateTechnologiesService).create(getCandidateTechnologiesDTO());
+            String candidateTechnologiesJson = new Gson().toJson(getCandidateTechnologiesDTO());
+            var candidateTechnology = getCandidateTechnologiesDTO();
+            mockMvc.perform(post("/ev-tec/candidate-technologies/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(candidateTechnologiesJson))
+                    .andExpect(status().isAccepted())
+                    .andExpect(result -> assertThrows(CandidateTechnologiesExistsException.class, () -> candidateTechnologiesService.create(candidateTechnology)));
+            verify(candidateTechnologiesService, atLeastOnce()).create(any(CandidateTechnologiesDTO.class));
+        }
     }
 
     @Test
@@ -88,16 +81,34 @@ class CandidateTechnologyControllerTests extends AbstractMVCTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    void findByIdOkTest() throws Exception {
-        var candidateTechnologies = getCandidateTechnologies();
-        String idJson = new Gson().toJson(getCandidateTechnologies().getId());
-        when(candidateTechnologiesService.findById(getCandidateTechnologies().getId())).thenReturn(candidateTechnologies);
-        mockMvc.perform(get("/ev-tec/candidate-technologies/findById/{candidateTechnologiesId}", idJson)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(idJson))
-                .andExpect(status().isOk());
-        verify(candidateTechnologiesService, times(1)).findById(candidateTechnologies.getId());
+    @Nested
+    class FindByIdTest {
+        @Test
+        void findByIdOkTest() throws Exception {
+            var candidateTechnologies = getCandidateTechnologies();
+            String idJson = new Gson().toJson(getCandidateTechnologies().getId());
+            when(candidateTechnologiesService.findById(getCandidateTechnologies().getId())).thenReturn(candidateTechnologies);
+            mockMvc.perform(get("/ev-tec/candidate-technologies/findById/{candidateTechnologiesId}", idJson)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(idJson))
+                    .andExpect(status().isOk());
+            verify(candidateTechnologiesService, times(1)).findById(candidateTechnologies.getId());
+        }
+
+        @Test
+        void findByIdWhenNotExistsTest() throws Exception {
+            doThrow(TechnologyNotExistsException.class).when(candidateTechnologiesService).findById(getCandidateTechnologiesDTO().getId());
+            var candidateTechnologies = getCandidateTechnologies();
+            var candidateTechnologiesId = getCandidateTechnologiesDTO().getId();
+            String idJson = new Gson().toJson(getCandidateTechnologies().getId());
+            when(candidateTechnologiesService.findById(getCandidateTechnologies().getId())).thenReturn(candidateTechnologies);
+            mockMvc.perform(get("/ev-tec/candidate-technologies/findById/{candidateTechnologiesId}", idJson)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(idJson))
+                    .andExpect(status().isOk())
+                    .andExpect(result -> assertThrows(TechnologyNotExistsException.class, () -> candidateTechnologiesService.findById(candidateTechnologiesId)));
+            verify(candidateTechnologiesService, times(1)).findById(candidateTechnologies.getId());
+        }
     }
 
     @Nested
@@ -131,29 +142,32 @@ class CandidateTechnologyControllerTests extends AbstractMVCTest {
         }
     }
 
-    @Test
-    void findCandidateByTechnologyTest() throws Exception {
-        var technologyName = getTechnology().getName();
-        String technologyNameJson = new Gson().toJson(getTechnology().getName());
-        when(technologyService.findByName(technologyName)).thenReturn(true);
-        mockMvc.perform(get("/ev-tec/candidate-technologies/listCandidateByTechnology/{technologyName}", technologyName)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(technologyNameJson))
-                .andExpect(status().isOk());
-        verify(candidateTechnologiesService, atLeastOnce()).listCandidateByTechnology(technologyName);
-    }
+    @Nested
+    class FindCandidateByTechnologyByNameTest {
+        @Test
+        void findCandidateByTechnologyTest() throws Exception {
+            var technologyName = getTechnology().getName();
+            String technologyNameJson = new Gson().toJson(getTechnology().getName());
+            when(technologyService.findByName(technologyName)).thenReturn(true);
+            mockMvc.perform(get("/ev-tec/candidate-technologies/listCandidateByTechnology/{technologyName}", technologyName)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(technologyNameJson))
+                    .andExpect(status().isOk());
+            verify(candidateTechnologiesService, atLeastOnce()).listCandidateByTechnology(technologyName);
+        }
 
-    @Test
-    void findCandidateByTechnologyNotFoundTest() throws Exception {
-        var technologyName = getTechnology().getName();
-        String technologyNameJson = new Gson().toJson(getTechnology().getName());
-        doThrow(TechnologyNotExistsException.class).when(candidateTechnologiesService).listCandidateByTechnology(technologyName);
-        when(technologyService.findByName(technologyName)).thenReturn(true);
-        mockMvc.perform(get("/ev-tec/candidate-technologies/listCandidateByTechnology/{technologyName}", technologyName)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(technologyNameJson))
-                .andExpect(status().isAccepted())
-                .andExpect(result -> assertThrows(TechnologyNotExistsException.class, () -> candidateTechnologiesService.listCandidateByTechnology(technologyName)));
-        verify(candidateTechnologiesService, atLeastOnce()).listCandidateByTechnology(technologyName);
+        @Test
+        void findCandidateByTechnologyNotFoundTest() throws Exception {
+            var technologyName = getTechnology().getName();
+            String technologyNameJson = new Gson().toJson(getTechnology().getName());
+            doThrow(TechnologyNotExistsException.class).when(candidateTechnologiesService).listCandidateByTechnology(technologyName);
+            when(technologyService.findByName(technologyName)).thenReturn(true);
+            mockMvc.perform(get("/ev-tec/candidate-technologies/listCandidateByTechnology/{technologyName}", technologyName)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(technologyNameJson))
+                    .andExpect(status().isAccepted())
+                    .andExpect(result -> assertThrows(TechnologyNotExistsException.class, () -> candidateTechnologiesService.listCandidateByTechnology(technologyName)));
+            verify(candidateTechnologiesService, atLeastOnce()).listCandidateByTechnology(technologyName);
+        }
     }
 }
