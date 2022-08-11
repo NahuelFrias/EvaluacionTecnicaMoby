476a7db53a5e37cc1fc0d74b39336b5a15a916d8
leonahuelfrias <leonahuelfrias@gmail.com>
2022-08-11 09:09:51 -0300

    Class Queries fixed

--- src/main/java/com/test/backend/developer/test_backend_nahuel/utils/Queries.java
+++ src/main/java/com/test/backend/developer/test_backend_nahuel/utils/Queries.java
@@ -2,7 +2,7 @@ package com.test.backend.developer.test_backend_nahuel.utils;
 
 public final class Queries {
     private Queries() {}
-    public static final String FIND_CANDIDATE_BY_DOCUMENT = "SELECT * FROM CANDIDATE c WHERE c.document = ?1";
+    public static final String FIND_CANDIDATE_BY_DOCUMENT = "SELECT * FROM CANDIDATE c WHERE c.num_document = ?1";
     public static final String FIND_TECHNOLOGY_NAME_AND_VERSION = "SELECT * FROM TECHNOLOGY t WHERE t.technology_name = ?1 AND t.version = ?2";
     public static final String FIND_BY_CANDIDATE_ID_AND_TECHNOLOGY_ID = "SELECT * FROM CANDIDATE_TECHNOLOGIES ct WHERE ct.candidate_id = ?1 AND ct.technology_id = ?2";
     public static final String FIND_CANDIDATE_BY_TECHNOLOGY = "SELECT c.name, c.lastname, c.document_type ,c.num_document, c.birthdate, t.technology_name, t.version, ct.experience " +
